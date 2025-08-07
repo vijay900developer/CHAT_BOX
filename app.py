ffd94d9c34708a12f0ba6574fee5cf1f132ca717
vijay900developer <vijaymis@cityvibes.in>
2025-08-07 13:51:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,4 +113,6 @@ def home():
     return "🤖 WhatsApp AI Chatbot is running!"
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
